373afa9fc8059e5bd663cb01085c7fc094cdd09e
Rohit Dasgupta <rdg922@gmail.com>
2021-10-16 15:59:18 -0400

    Create lesson-example-reference.py

---
+++ lesson-example-reference.py
@@ -0,0 +1,3 @@
+# This is a comment! It will not print anything when you run. Copy and paste this code into your replit and press run to see nothing happening
+# Nothing should happen on the right! Make a comment with your name on it by adding a line and starting a line with the # key:
+# Sensei Ro (Your name will probably differ!)
